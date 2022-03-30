58bd7550391a9b6721f36081b72b91fbb45faf3b
Freja <frej400d@stud.kea.dk>
2022-03-30 03:21:04 +0200

    first commit

---
+++ scripts/projects.js
@@ -0,0 +1,93 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("Welcome");
+  let ntl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
+  let goBack = document.querySelector("#go-back-link");
+
+  ntl.to(goBack, { duration: 0.2, opacity: 0, scale: 0.6 });
+  ntl.to(goBack, { duration: 0.5, opacity: 1, scale: 1 });
+  let goBackGrow = gsap
+    .timeline({
+      repeat: Infinity,
+      repeatDelay: 5,
+      scrollTrigger: {
+        trigger: ".first-detail",
+        start: "20% 70%",
+        markers: true,
+      },
+    })
+    .to(goBack, { duration: 0.1, y: "0%" })
+    .to(goBack, { duration: 1.5, y: "-2rem", ease: Expo.easeOut })
+    .to(goBack, { duration: 0.2, rotation: 5, transformOrigin: "top 50%" })
+    .to(goBack, { duration: 0.2, rotation: -5, transformOrigin: "top 50%" })
+    .to(goBack, { duration: 0.2, rotation: 5, transformOrigin: "top 50%" })
+    .to(goBack, { duration: 0.2, rotation: -5, transformOrigin: "top 50%" })
+    .to(goBack, { duration: 2, y: "0%", rotation: 0, ease: Bounce.easeOut });
+  //.to(goBack, { duration: 1.2, scale: 1.2 })
+  //.to(goBack, { duration: 1.2, scale: 1 });
+
+  goBack.addEventListener("mouseenter", () => goBackGrow.pause());
+  goBack.addEventListener("mouseleave", () => goBackGrow.resume());
+  headerAnimation();
+}
+
+function headerAnimation() {
+  let pHeaderText = document.querySelector(".project-header");
+  let tl = gsap.timeline({ repeat: 0, repeatDelay: 0 });
+  tl.to(pHeaderText, { duration: 0.2, opacity: 0, scale: 0.6 });
+  tl.to(pHeaderText, { duration: 0.5, opacity: 1, scale: 1 });
+  //tl.to(goBack, 0.5, { css: { className: "+=wiggle" } });
+  animations();
+}
+
+function animations() {
+  gsap.to(".mockup-left", {
+    delay: 0.75,
+    x: "0%",
+    opacity: 1,
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".project-page",
+      start: "top 70%",
+    },
+  });
+
+  gsap.to(".mockup-right", {
+    delay: 0.75,
+    x: "0%",
+    opacity: 1,
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".project-page",
+      start: "top 70%",
+    },
+  });
+
+  gsap.to(".first-detail", {
+    x: "0%",
+    opacity: 1,
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".mockup",
+
+      start: "60% center",
+    },
+  });
+
+  gsap.to(".second-detail", {
+    x: "0%",
+    opacity: 1,
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: ".first-detail",
+      start: "30% 40%",
+    },
+  });
+}
---
+++ scripts/script.js
@@ -0,0 +1,101 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("Welcome to my Portfolio");
+  gsap.to("progress", {
+    value: 100,
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#content",
+      scrub: 0.3,
+      end: "90% 70%",
+    },
+  });
+  let headerImg = document.querySelector(".header-img");
+  let headerText = document.querySelector(".header-text");
+  let arrow = document.querySelector("#arrow");
+  let tl = gsap.timeline({ repeat: 0, repeatDelay: 1 });
+  tl.to(headerImg, { duration: 0.1, opacity: 0, scale: 0.6 });
+  tl.to(headerImg, { duration: 1.2, opacity: 1, scale: 1 });
+  tl.to(headerText, { duration: 0.2, opacity: 0, y: "5rem" });
+  tl.to(headerText, { duration: 0.5, opacity: 1, y: "-2rem" });
+  tl.to(headerText, { duration: 0.8, opacity: 1, ease: Bounce.easeOut, y: 0 });
+  tl.to(headerImg, {
+    duration: 1,
+    boxShadow: "0 0 40px 40px #ff2986",
+  });
+  tl.to(arrow, 0.5, { css: { className: "+=bouncing" } });
+
+  frontpageScrollAnimations();
+}
+function frontpageScrollAnimations() {
+  gsap.to(".about-texts", {
+    x: "0%",
+    opacity: 1,
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#about",
+      start: "top 70%",
+    },
+  });
+
+  gsap.to(".about-image", {
+    x: "0%",
+    opacity: 1,
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#about",
+      start: "top 70%",
+    },
+  });
+
+  gsap.to(".project-heading", {
+    opacity: 1,
+    duration: 1.5,
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: "#about",
+      scrub: 0.5,
+      start: "bottom 70%",
+      end: "150% 70%",
+    },
+  });
+
+  let cardContainer = document.querySelector(".card-container");
+  gsap.to(cardContainer, {
+    duration: 3,
+    y: "-10vw",
+    opacity: 1,
+    scrollTrigger: {
+      trigger: "#projects",
+      scrub: 0.5,
+      start: "top 50%",
+      end: "10% 50%",
+    },
+  });
+
+  let circle = document.querySelector(".contact-circle");
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "20% 70%",
+      },
+    })
+
+    .to(circle, {
+      y: "-20%",
+      duration: 0.5,
+    })
+
+    .to(circle, {
+      duration: 0.8,
+      opacity: 1,
+      ease: Bounce.easeOut,
+      y: 0,
+    });
+}
