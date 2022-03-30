0d36c988e757f39b838c8ef776621c2bedf69b33
Freja <frej400d@stud.kea.dk>
2022-03-30 16:48:12 +0200

    cleo header

--- scripts/cleo.js
+++ scripts/cleo.js
@@ -4,15 +4,29 @@ window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   console.log("Hello this is my dog");
-  /*   let goBack = document.querySelector(".go-back-ani");
-  gsap.to(goBack, {
-    delay: 1.1,
-    duration: 2,
-    y: "0%",
-    opacity: 1,
+  let cleoImg = document.querySelector(".header-cleo-img");
+  let cleoText = document.querySelector(".cleo-text");
+  let galleryH = document.querySelector("h2");
+  let tl = gsap.timeline({ repeat: 0, repeatDelay: 1 });
+  tl.to(cleoImg, {
+    duration: 2.5,
+    x: "-5%",
+    rotation: -190,
+    transformOrigin: "center",
     ease: Power2.easeOut,
-  }); */
-
+  });
+  tl.to(cleoImg, {
+    duration: 1.5,
+    x: "0%",
+    rotation: -180,
+    transformOrigin: "center",
+    ease: Bounce.easeOut,
+  });
+  tl.to(cleoText, { duration: 0.2, opacity: 0, y: "5rem" });
+  tl.to(cleoText, { duration: 0.5, opacity: 1, y: "-2rem" });
+  tl.to(cleoText, { duration: 0.8, opacity: 1, ease: Bounce.easeOut, y: 0 });
+  tl.to(galleryH, { duration: 0.1, opacity: 0 });
+  tl.to(galleryH, { duration: 1, opacity: 1 });
   frontpageScrollAnimations();
 }
 function frontpageScrollAnimations() {
@@ -52,4 +66,13 @@ function frontpageScrollAnimations() {
       end: "bottom 40%",
     },
   });
+
+  /*   let goBack = document.querySelector(".go-back-ani");
+  gsap.to(goBack, {
+    delay: 1.1,
+    duration: 2,
+    y: "0%",
+    opacity: 1,
+    ease: Power2.easeOut,
+  }); */
 }
