5e9ce1657bfd10736f12a6b3f796894e735eba9c
Freja <frej400d@stud.kea.dk>
2022-03-30 16:04:45 +0200

    gallery

--- scripts/cleo.js
+++ scripts/cleo.js
@@ -4,6 +4,18 @@ window.addEventListener("DOMContentLoaded", start);
 
 function start() {
   console.log("Hello this is my dog");
+  /*   let goBack = document.querySelector(".go-back-ani");
+  gsap.to(goBack, {
+    delay: 1.1,
+    duration: 2,
+    y: "0%",
+    opacity: 1,
+    ease: Power2.easeOut,
+  }); */
+
+  frontpageScrollAnimations();
+}
+function frontpageScrollAnimations() {
   let cleoFirstSection = document.querySelector("#cleo-first-section");
   let cleoSecondSection = document.querySelector("#cleo-second-section");
   let cleoThirdSection = document.querySelector("#cleo-third-section");
@@ -32,16 +44,12 @@ function start() {
 
   gsap.to(cleoThirdSection, {
     duration: 3,
-    x: "18%",
+    x: "8%",
     scrollTrigger: {
       trigger: "#cleo-third-section",
       scrub: 0.5,
-      start: "-20% 50%",
-      end: "120% 50%",
-      markers: true,
+      start: "-20% 80%",
+      end: "bottom 40%",
     },
   });
-
-  frontpageScrollAnimations();
 }
-function frontpageScrollAnimations() {}
