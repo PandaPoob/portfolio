78d39f3bf8a7d59a21cc6881d6e1bee118b9d22e
Freja <frej400d@stud.kea.dk>
2022-03-31 00:03:01 +0200

    expand images

---
+++ scripts/game_popup.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("Welcome");
+  document
+    .querySelector("#game-open-popup")
+    .addEventListener("click", () => showFxImg());
+}
+
+function showFxImg() {
+  console.log("click");
+  const gamePopup = document.querySelector("#game-popup");
+  gamePopup.style.display = "block";
+
+  const container = document.querySelector(".popup-container");
+  container.addEventListener("click", closeGameImg);
+}
+
+function closeGameImg() {
+  document.querySelector("#game-popup").style.display = "none";
+  document
+    .querySelector("#game-popup")
+    .removeEventListener("click", closeGameImg);
+}
